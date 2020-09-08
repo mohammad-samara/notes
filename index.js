2023bd6724520e239a7296dbb74adeba88baa7fc
mohammad-samara <mohammad.ab.samara@gmail.com>
2020-09-08 18:15:27 +0300

    done without testing

--- index.js
+++ index.js
@@ -8,12 +8,22 @@
 
 const Input = require('./lib/input.js');  //take inputs(note)
 const Notes = require('./lib/notes.js');  //display input(note)
+const mongoose = require('mongoose');
+const MONGOOSE_URL = 'mongodb://localhost:27017/food';
+
+mongoose.connect(MONGOOSE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 const inputCommand = new Input();
+// console.log('the inputCommand to be processed : ',inputCommand);
 const myNote = new Notes(inputCommand);
 inputCommand.valid() ? myNote.execute(inputCommand) : help() ;
 
-function help() {
+async function help() {
   console.log(`
         api usage1: api --add <text note>
         api usage2: api -a <text note>
@@ -23,5 +33,7 @@ function help() {
         must enter a text note
         must use -a or --add
     `);
+  await mongoose.disconnect();
 }
+
 //myNote.execute(inputCommand);
